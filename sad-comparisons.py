0f03c66473187cd56ab228b6d780bb602d9007e6
Elita Baldridge <elita.baldridge@weecology.org>
2014-07-11 11:29:40 -0500

    Removes extra whitespace in weights_output.insert(3, '').

--- sad-comparisons.py
+++ sad-comparisons.py
@@ -132,7 +132,7 @@ def model_comparisons(raw_data, dataset_name, data_dir, cutoff = 9):
             
             # Inserts a blank in the output if the negative binomial exceeded the max number of iterations
             if negbin_blank == 1:
-                weights_output. insert(3, '')
+                weights_output.insert(3, '')
             
             # Inserts a blank in the output if the generalized Yule failed to converge    
             if gen_yule_blank == 1:
