c9101000aa0c6f33c86eb78ac9fb246af0aae3be
Elita Baldridge <elita.baldridge@weecology.org>
2014-10-26 16:09:34 -0500

    Adds start of turning the .csv files into a queryable database.

--- data-cleaning.py
+++ data-cleaning.py
@@ -5,3 +5,18 @@ import multiprocessing
 import os
 import numpy as np
 import sqlite3 as dbapi
+
+#Stuff data into a real database
+data_dir = './sad-data/chapter2' # path to data directory
+
+
+# Set up database capabilities 
+# Set up ability to query data
+con = dbapi.connect('./sad-data/UlrichOllik2003.sqlite')
+cur = con.cursor()
+    
+# Switch con data type to string
+con.text_factory = str    
+cur.execute("""DROP TABLE IF EXISTS summary""")
+cur.execute("""DROP TABLE IF EXISTS abundance""")
+con.commit() 
