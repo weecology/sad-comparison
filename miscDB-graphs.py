bf8246ca2e0f8d788db91b20cdfd41c8e6fb59aa
Elita Baldridge <elita.baldridge@weecology.org>
2015-01-23 15:17:31 -0600

    Adds graphs by taxa for total individuals and number of sites.

--- miscDB-graphs.py
+++ miscDB-graphs.py
@@ -38,13 +38,33 @@ bioregions = cur.execute("""SELECT DISTINCT(biogeographic_region) AS region, COU
                             miscabundancedb_sites
                             GROUP BY region""")
 bioregions = cur.fetchall()
-
 #Make bar graph of sites per biogeographic region
 bioregions_graph = './sad-data/chapter2/bioregions.png'
 ylabel_name = "Number of sites"
 xlabel_name = "Biogeographic regions"
 bar_graph(bioregions_graph, bioregions, ylabel_name, xlabel_name)
 
+#Count the number of individuals for each taxon
+num_taxa = cur.execute("""SELECT DISTINCT(class) AS class, COUNT(species) AS count_individuals FROM
+                            miscabundancedb_main
+                            GROUP BY class""")
+num_taxa = cur.fetchall()
+#Make bar graph of number of individuals for each taxon
+taxa_graph = './sad-data/chapter2/num_taxa.png'
+ylabel_name = "Total individuals"
+xlabel_name = "Class"
+bar_graph(taxa_graph, num_taxa, ylabel_name, xlabel_name)
+
+#Count the number of sites for each taxon
+sites_taxa = cur.execute("""SELECT DISTINCT(class) AS class, COUNT(DISTINCT(site_id)) AS sites FROM
+                            miscabundancedb_main
+                            GROUP BY class""")
+sites_taxa = cur.fetchall()
+#Make bar graph of number of sites for each taxon
+taxa_sites_graph = './sad-data/chapter2/taxa_sites.png'
+ylabel_name = "Number of sites"
+xlabel_name = "Class"
+bar_graph(taxa_sites_graph, sites_taxa, ylabel_name, xlabel_name)
 
 
 #Close connection
