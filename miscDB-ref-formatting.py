f3b5ca72fd07c8c90f2d14f28893c55c8af0c1fc
Elita Baldridge <elita.baldridge@weecology.org>
2015-01-26 16:04:37 -0600

    Adds function to automate creation of .bib file.

--- miscDB-ref-formatting.py
+++ miscDB-ref-formatting.py
@@ -9,15 +9,18 @@ import re
 
 sys.float_info[2]
 
+#Make reference
+def bib_reference(ref_num, title, author, journal, volume, number, pages, year, publisher):
+    ref_entry = '@article{' + ref_num + ', title={' + title + '}, author={' + author + '}, journal={' + journal + '}, volume={' + volume + '}, number={' + number + '}, pages={' + pages + '}, year={' + year + '}, publisher={' + publisher + '}}'
+    
+    with open('./sad-data/chapter2/miscDB_refs.bib','w') as archive_file:
+        refs = write(archive_file)
+        refs.writelines(ref_entry)
+
+
 # Set up database capabilities 
 # Set up ability to query data
 con = dbapi.connect('./sad-data/chapter2/misc.sqlite')
 cur = con.cursor()
 # Switch con data type to string
 con.text_factory = str 
-
-#Setup output file
-refs_file = './sad-data/chapter2/miscDB_refs.bib'
-
-with open(refs_file,'wb') as archive_file:
-    refs = csv.writer(archive_file)
