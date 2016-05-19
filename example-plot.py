7cb9aa6d29435604b58d8901d93d99ea5388b4ed
David J. Harris <harry491@gmail.com>
2016-05-19 15:35:19 -0400

    triple example plot

--- example-plot.py
+++ example-plot.py
@@ -6,53 +6,67 @@ import matplotlib
 import matplotlib.pyplot as plt
 %matplotlib inline
 
-ab = np.array([1L, 4L, 2L, 17L, 23L, 1L, 7L, 2L, 5L, 1L, 5L, 2L, 4L, 13L,
+# BBS
+# fia site 14057000012
+# Gentry site 95
+ab_array = np.array([[1L, 4L, 2L, 17L, 23L, 1L, 7L, 2L, 5L, 1L, 5L, 2L, 4L, 13L,
 1L, 4L, 7L, 11L, 12L, 2L, 1L, 17L, 35L, 16L, 88L, 10L, 2L, 1L,
 15L, 52L, 10L, 1L, 1L, 14L, 1L, 1L, 9L, 2L, 17L, 105L, 9L, 7L,
 45L, 14L, 2L, 7L, 24L, 15L, 4L, 2L, 3L, 9L, 7L, 10L, 3L, 29L,
 1L, 2L, 7L, 11L, 9L, 5L, 22L, 2L, 27L, 14L, 1L, 2L, 11L, 4L,
-21L, 114L, 22L, 44L, 9L, 5L, 2L, 2L, 6L, 16L, 10L])
-x_values = np.array(range(max(ab) + 5)[1:])
+21L, 114L, 22L, 44L, 9L, 5L, 2L, 2L, 6L, 16L, 10L],
+              [2L, 1L, 1L, 2L, 1L, 2L, 1L, 5L, 1L, 1L, 1L, 4L, 7L, 2L, 1L,
+2L, 1L, 3L, 5L, 1L, 1L],
+             [4L, 22L, 7L, 12L, 2L, 6L, 2L, 15L, 5L, 6L, 1L, 3L, 1L, 2L,
+3L, 6L, 1L, 3L, 2L, 3L, 9L, 5L, 8L, 2L, 5L, 1L, 5L, 7L, 4L, 1L,
+4L, 3L, 4L, 1L, 2L, 3L, 11L, 5L, 7L, 3L, 1L, 2L, 1L, 1L, 1L,
+4L, 1L, 1L, 9L, 3L, 8L, 3L, 2L, 10L, 4L, 14L, 4L, 2L, 3L, 1L,
+1L, 8L]])
 
-logser_p = md.logser_solver(ab)
-logser_values = md.trunc_logser.pmf(x_values, logser_p, upper_bound=float("inf"))
-lsll = md.logser_ll(ab, logser_p)
+# rearranged from http://www.cookbook-r.com/Graphs/Colors_%28ggplot2%29/#a-colorblind-friendly-palette
+colors = ["#F0E442", "#D55E00", "#E69F00", "#56B4E9", "#009E73", "#0072B2", "#999999", "#CC79A7"]
 
-nb_n, nb_p = md.nbinom_lower_trunc_solver(ab)
-nb_values = md.nbinom_lower_trunc.pmf(x_values, nb_n, nb_p)
-nbll = md.nbinom_lower_trunc_ll(ab, nb_n, nb_p)
+for i in range(3):
+    ab = ab_array[i]
 
-pln_mu, pln_sigma = md.pln_solver(ab)
-pln_values = md.pln.pmf(x_values, pln_mu, pln_sigma, lower_trunc=True)
-plnll = md.pln_ll(ab, pln_mu, pln_sigma)
+    x_values = np.array(range(max(ab) + 2)[1:])
 
-zipf_par = md.zipf_solver(ab)
-zipf_values = zipf.pmf(x_values, zipf_par)
-zll = md.zipf_ll(ab, zipf_par)
+    logser_p = md.logser_solver(ab)
+    logser_values = md.trunc_logser.pmf(x_values, logser_p, upper_bound=float("inf"))
+    lsll = md.logser_ll(ab, logser_p)
 
-ab_y = np.zeros(len(x_values) + 1)
-for i in range(len(ab)):
-    ab_y[ab[i]] = ab_y[ab[i]] + 1/len(ab)
+    nb_n, nb_p = md.nbinom_lower_trunc_solver(ab)
+    nb_values = md.nbinom_lower_trunc.pmf(x_values, nb_n, nb_p)
+    nbll = md.nbinom_lower_trunc_ll(ab, nb_n, nb_p)
 
-fig= plt.figure(figsize=(15, 10), dpi=800)
-ax1 = fig.add_subplot(111)
-ax1.set_xlim([0,50])
-ax1.set_ylim([0,max(ab_y) * 1.25])
+    pln_mu, pln_sigma = md.pln_solver(ab)
+    pln_values = md.pln.pmf(x_values, pln_mu, pln_sigma, lower_trunc=True)
+    plnll = md.pln_ll(ab, pln_mu, pln_sigma)
 
-plt.ylabel('frequency')
-plt.xlabel('abundance')
+    zipf_par = md.zipf_solver(ab)
+    zipf_values = zipf.pmf(x_values, zipf_par)
+    zll = md.zipf_ll(ab, zipf_par)
 
-ax1.vlines(x = x_values, ymin = [0], ymax = .0025, color = "#999999")
-ax1.vlines(x = x_values, ymin=[0], ymax=ab_y[1:], linewidth = 12, color = "#999999")
-ax1.plot(x_values, logser_values, "k-", label = "log-series: " + str(round(lsll)), linewidth = 1.5)
-ax1.plot(x_values, nb_values, "b-", label = "negative binomial: " + str(round(nbll)), linewidth = 1.5)
-ax1.plot(x_values, pln_values, "r-", label = "Poisson-lognormal: " + str(round(plnll)), linewidth = 1.5)
-ax1.plot(x_values, zipf_values, "m-", label = "zipf: " + str(round(zll)), linewidth = 1.5)
-plt.legend(loc='upper right');
+    ab_y = np.zeros(len(x_values) + 1)
+    for i in range(len(ab)):
+        ab_y[ab[i]] = ab_y[ab[i]] + 1/len(ab)
 
-# Notes:  I'm only showing 1:50 on the x axis because letting the axis go out to 114 would only show 4 more points
-#          but would take twice as much space.
-#         Zipf spends too much probability mass at both extremes and not enough in the middle, where most of the
-#          values are. It gets essentially no weight.
-#         NB and PLN fit best, but only by 2 LL points. This isn't enough to overcome the AICc penalty relative
-#          to log-series, which is always at least 2
+    fig= plt.figure(figsize=(12, 8), dpi=800)
+    ax1 = fig.add_subplot(111)
+    ax1.set_xlim([0,min(50, max(x_values))])
+    #ax1.set_ylim([0,max(ab_y) * 1.25])
+
+    plt.ylabel('frequency')
+    plt.xlabel('abundance')
+
+    # Width originally set at 12 when width was 50.
+    # This should be the same proportional width
+    width = 12 / min(50, max(x_values)) * 50
+
+    ax1.vlines(x = x_values, ymin = [0], ymax = .0025, color = "#999999")
+    ax1.vlines(x = x_values, ymin=[0], ymax=ab_y[1:], linewidth = width, color = "#999999")
+    ax1.plot(x_values, logser_values, color = colors[1], label = "log-series: " + str(round(lsll)), linewidth = 1.5)
+    ax1.plot(x_values, nb_values, color = colors[3], label = "negative binomial: " + str(round(nbll)), linewidth = 1.5)
+    ax1.plot(x_values, pln_values, color = colors[7], label = "Poisson-lognormal: " + str(round(plnll)), linewidth = 1.5)
+    ax1.plot(x_values, zipf_values, color = "k", label = "zipf: " + str(round(zll)), linewidth = 1.5)
+    plt.legend(loc='upper right');
