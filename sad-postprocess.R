5c7204adceb736dd8cb73a43c255643a70fcdac8
David J. Harris <harry491@gmail.com>
2016-11-10 15:14:57 -0500

    more color

--- sad-postprocess.R
+++ sad-postprocess.R
@@ -2,6 +2,9 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(magrittr)
+library(dichromat)
+
+colors = colorschemes$SteppedSequential.5[c(1, 6, 11, 16)]
 
 if (!dir.exists("violins")) {
   dir.create("violins")
@@ -172,25 +175,28 @@ AICc_diff = (deviances[, is_AICc] - rowMeans(deviances[, is_AICc])) %>%
   fancify_names()
 AICc_diff_long = gather(AICc_diff, key = "Distribution", value = "AICc")
 
-dev_plot = ggplot(deviance_diff_long, aes(x = Distribution, y = deviance)) + 
+dev_plot = ggplot(deviance_diff_long, aes(x = Distribution, y = deviance, fill = Distribution)) + 
   geom_hline(yintercept = 0) + 
   geom_violin() + 
   theme_bw() + 
+  scale_fill_manual(values = colors, guide = FALSE) +
   ylab("Deviation from mean deviance (lower is better)")
 my_ggsave(name = "deviance.png", dev_plot)
 
 
-ll_plot = ggplot(deviance_diff_long, aes(x = Distribution, y = -deviance/2)) + 
+ll_plot = ggplot(deviance_diff_long, aes(x = Distribution, y = -deviance/2, fill = Distribution)) + 
   geom_hline(yintercept = 0) + 
   geom_violin() + 
   theme_bw() + 
+  scale_fill_manual(values = colors, guide = FALSE) +
   ylab("Deviation from mean log-likelihood")
 my_ggsave(name = "loglik.png", ll_plot)
 
-aic_plot = ggplot(AICc_diff_long, aes(x = Distribution, y = AICc)) + 
+aic_plot = ggplot(AICc_diff_long, aes(x = Distribution, y = AICc, fill = Distribution)) + 
   geom_hline(yintercept = 0) + 
   geom_violin() + 
   theme_bw() + 
+  scale_fill_manual(values = colors, guide = FALSE) +
   ylab("Deviation from mean AICc (lower is better)")
 my_ggsave(name = "aic.png", aic_plot)
 
@@ -212,16 +218,18 @@ AICc_weight_long = gather(AICc_weight,
 # Note: I had to tweak the bandwidth parameter for this plot, or zipf's splat at
 # zero would be so wide that the other distributions would be invisible by comparison.
 # A bandwidth much less than 0.01 on a 0-1 scale is probably undersmoothed anyway.
-relative_plot = ggplot(relative_likelihoods_long, aes(x = Distribution, y = relative_likelihood)) +
+relative_plot = ggplot(relative_likelihoods_long, aes(x = Distribution, y = relative_likelihood, fill = Distribution)) +
   geom_violin(bw = .01) +
   theme_bw() +
   coord_cartesian(ylim = c(0, 1), expand = FALSE) + 
+  scale_fill_manual(values = colors, guide = FALSE) +
   ylab("Relative likelihood (higher is better)")
 my_ggsave("relative.png", relative_plot)
 
-weight_plot = ggplot(AICc_weight_long, aes(x = Distribution, y = AICc_weight)) +
+weight_plot = ggplot(AICc_weight_long, aes(x = Distribution, y = AICc_weight, fill = Distribution)) +
   geom_violin(bw = .01) +
   theme_bw() +
+  scale_fill_manual(values = colors, guide = FALSE) +
   coord_cartesian(ylim = c(0, 1), expand = FALSE) + 
   ylab("AICc weight (higher is better)")
 my_ggsave("weight.png", weight_plot)
@@ -276,7 +284,7 @@ apply(AICc_diff, 1, which.min) %>%
   get_names() %>%
   table() %>% 
   barplot(ylab = "Number of wins", xlab = "Species abundance distribution",
-          las = 1, space = 0)
+          las = 1, space = 0, col = colors)
 dev.off()
   
 winners_df = deviances %>% 
@@ -287,14 +295,12 @@ winners_df = deviances %>%
   mutate(panel = LETTERS[1 + (N < 1E3) + (N < 1E2)]) %>% 
   mutate(winner = fancify(winner))
 
-library(dichromat)
-
 ggplot(winners_df, aes(x = winner, y = wins, fill = factor(winner))) + 
   geom_bar(stat = "identity") +
   facet_wrap(~id, nrow = 4, scales = "free_y") + 
-  cowplot::theme_cowplot() + 
+  theme_bw() + 
   xlab("Species abundance distribution") + 
   ylab("Number of wins") + 
-  scale_fill_manual(values = colorschemes$SteppedSequential.5[c(1, 6, 11, 16)],
+  scale_fill_manual(values = colors,
                     guide = FALSE)
 ggsave("wins-by-dataset.png", width = 11, height = 6)
